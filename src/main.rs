06244c2d7fc42665007025863b236dd5ef6f19fe
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2024-01-17 11:44:20 +0000

    add export to Integer (rug)

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,23 @@ use gmp_mpfr_sys::gmp;
 use num_bigint::BigUint;
 use std::ffi::c_void;
 use std::ptr::NonNull;
+use rug::Integer;
 
 fn main() {
+        // Create an Integer from a string
+        // let rug_integer = Integer::from_str_radix("12345678901234567890", 10).unwrap();
+        // println!("Rug Integer: {}", rug_integer);
+        // println!("Rug integer multiplied by 2: {}", rug_integer * 2);
+
         let mut mpz = gmp::mpz_t { alloc: 0, size: 0, d: NonNull::dangling() };
         unsafe {gmp::mpz_init_set_str(&mut mpz, b"12345678901234567890\0".as_ptr() as *const i8, 10) };
+
+        // Create an Integer from the mpz_t pointer
+        let rug_integer2 = unsafe { Integer::from_raw(mpz) };
+        let rug_integer3 = rug_integer2.clone();
+        println!("Rug Integer: {}", rug_integer2);
+        println!("Rug integer multiplied by 2: {}", rug_integer3 * 2);
+
 
         // We can calculate the size of the buffer needed to export the mpz
         //let size = unsafe { gmp::mpz_sizeinbase(&mpz, 256) } as usize;
@@ -26,9 +39,6 @@ fn main() {
             0,
             &mut mpz,
         ); }
-
-        // Resize the buffer based on the actual count
-        buffer.resize(count, 0);
 
         // Convert the raw bytes to a BigUint
         let big_uint = BigUint::from_bytes_be(&buffer[..count]);
